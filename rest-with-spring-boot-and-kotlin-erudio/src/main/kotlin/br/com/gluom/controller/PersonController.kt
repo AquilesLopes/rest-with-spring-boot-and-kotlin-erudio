2e39425aa522b2956e12857482d585a95103d175
Aquiles Lopes <aquiles_oliveira@live.com>
2022-06-30 13:44:18 -0300

    add consumes and produces JSON, XML, YML

--- rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/gluom/controller/PersonController.kt
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/gluom/controller/PersonController.kt
@@ -2,8 +2,8 @@ package br.com.gluom.controller
 
 import br.com.gluom.data.vo.v1.PersonVO
 import br.com.gluom.services.PersonService
+import br.com.gluom.util.MediaType
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -14,29 +14,29 @@ class PersonController {
     @Autowired
     private lateinit var personService: PersonService
 
-    @GetMapping(produces=[MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(produces=[MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
     fun findAll(): List<PersonVO> {
         return personService.findAll()
     }
 
-    @GetMapping(value=["/{id}"], produces=[MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value=["/{id}"], produces=[MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
     fun findById(@PathVariable(value="id") id: Long): PersonVO {
         return personService.findById(id)
     }
 
-    @PostMapping(consumes=[MediaType.APPLICATION_JSON_VALUE],
-                 produces=[MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(consumes=[MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML],
+                 produces=[MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
     fun create(@RequestBody person: PersonVO): PersonVO {
         return personService.create(person)
     }
 
-    @PutMapping(consumes=[MediaType.APPLICATION_JSON_VALUE],
-                produces=[MediaType.APPLICATION_JSON_VALUE])
+    @PutMapping(consumes=[MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML],
+                produces=[MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
     fun update(@RequestBody person: PersonVO): PersonVO {
         return personService.update(person)
     }
 
-    @DeleteMapping(value=["/{id}"], produces=[MediaType.APPLICATION_JSON_VALUE])
+    @DeleteMapping(value=["/{id}"], produces=[MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML])
     fun delete(@PathVariable(value="id") id: Long) : ResponseEntity<*> {
         personService.delete(id)
         return ResponseEntity.noContent().build<Any>()
---
+++ rest-with-spring-boot-and-kotlin-erudio/src/main/kotlin/br/com/gluom/util/MediaType.kt
@@ -0,0 +1,12 @@
+package br.com.gluom.util
+
+object MediaType {
+    const val APPLICATION_JSON = "application/json"
+    const val APPLICATION_XML = "application/xml"
+    const val APPLICATION_YML = "application/x-yaml"
+    const val TEXT_PLAIN = "text/plain"
+    const val TEXT_XML = "text/xml"
+    const val TEXT_HTML = "text/html"
+    const val IMAGE_JPEG = "image/jpeg"
+    const val IMAGE_PNG = "image/png"
+}
